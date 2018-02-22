b208f44f3bbda87a3afd13e6463e6f6be8c2a38d
h_type <din.ranger@yandex.ru>
2018-02-22 21:07:33 +0500

    Add stylelint support

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,42 +1,29 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "node": true,
-        "es6": true
+    env: {
+        browser: true,
+        node: true,
+        es6: true
     },
-    "extends": ["eslint:recommended", "plugin:react/recommended", "prettier"],
-    "parserOptions": {
-        "ecmaFeatures": {
-            "experimentalObjectRestSpread": true,
-            "jsx": true
+    extends: ['eslint:recommended', 'plugin:react/recommended', 'prettier'],
+    parserOptions: {
+        ecmaFeatures: {
+            experimentalObjectRestSpread: true,
+            jsx: true
         },
-        "sourceType": "module"
+        sourceType: 'module'
     },
-    "plugins": [
-        "react",
-        "prettier"
-    ],
-    "settings": {
-        "react": {
-            "pragma": "h"
+    plugins: ['react', 'prettier'],
+    settings: {
+        react: {
+            pragma: 'h'
         }
     },
-    "rules": {
-        "indent": 0,
-        "linebreak-style": [
-            "error",
-            "unix"
-        ],
-        "quotes": [
-            "error",
-            "single",
-            "avoid-escape"
-        ],
-        "semi": [
-            "error",
-            "always"
-        ],
-        "react/prop-types": [0],
-        "prettier/prettier": "error"
+    rules: {
+        indent: 0,
+        'linebreak-style': ['error', 'unix'],
+        quotes: ['error', 'single', 'avoid-escape'],
+        semi: ['error', 'always'],
+        'react/prop-types': [0],
+        'prettier/prettier': 'error'
     }
 };
