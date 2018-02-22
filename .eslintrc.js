b0f431afd505fc4ff31424a63401f92ee82c8215
h_type <din.ranger@yandex.ru>
2018-02-22 21:03:48 +0500

    Add eslint support

---
+++ .eslintrc.js
@@ -0,0 +1,42 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "node": true,
+        "es6": true
+    },
+    "extends": ["eslint:recommended", "plugin:react/recommended", "prettier"],
+    "parserOptions": {
+        "ecmaFeatures": {
+            "experimentalObjectRestSpread": true,
+            "jsx": true
+        },
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react",
+        "prettier"
+    ],
+    "settings": {
+        "react": {
+            "pragma": "h"
+        }
+    },
+    "rules": {
+        "indent": 0,
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "single",
+            "avoid-escape"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ],
+        "react/prop-types": [0],
+        "prettier/prettier": "error"
+    }
+};
