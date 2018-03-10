10b8e9858ef1a93690ad97d6acea17e67f71ec77
h_type <din.ranger@yandex.ru>
2018-03-10 16:10:18 +0500

    Fix placeholder styles

--- src/components/NoteList/Placeholder.js
+++ src/components/NoteList/Placeholder.js
@@ -7,8 +7,19 @@ import SaveNoteButton from 'components/Note/SaveButton';
 import DeleteNoteAllButton from 'components/NoteList/DeleteButton';
 import { splitClasses } from 'utils/className';
 
-const PlaceholderParagraph = ({ children }) => (
+const Paragraph = ({ children }) => (
     <div className={style.placeholder__paragraph}>{children}</div>
+);
+
+const ParagraphWithIllustration = ({ children }) => (
+    <div
+        className={splitClasses([
+            style.placeholder__paragraph,
+            style['placeholder__paragraph_with-illustration']
+        ])}
+    >
+        {children}
+    </div>
 );
 
 const Placeholder = ({ listType }) => (
@@ -18,35 +29,44 @@ const Placeholder = ({ listType }) => (
             style.placeholder
         ])}
     >
-        <PlaceholderParagraph>There are no notes yet.</PlaceholderParagraph>
-        <PlaceholderParagraph>
-            You can use buttons at the bottom left:
-        </PlaceholderParagraph>
-        <PlaceholderParagraph>
-            <AddButton
-                className={splitClasses([
-                    style[listType],
-                    style.placeholder__button
-                ])}
-                disabled
-            />{' '}
-            - to create note;
-        </PlaceholderParagraph>
-        <PlaceholderParagraph>
-            <DeleteNoteAllButton
-                className={style.placeholder__button}
-                disabled
-            />{' '}
-            - to delete all notes;
-        </PlaceholderParagraph>
-        <PlaceholderParagraph>
-            <DeleteNoteButton className={style.placeholder__button} disabled />{' '}
-            - to delete note;
-        </PlaceholderParagraph>
-        <PlaceholderParagraph>
-            <SaveNoteButton className={style.placeholder__button} disabled /> -
-            to save note text.
-        </PlaceholderParagraph>
+        <div className={style.placeholder__container}>
+            <Paragraph>There are no notes yet.</Paragraph>
+            <Paragraph>You can use buttons:</Paragraph>
+            <ParagraphWithIllustration>
+                <AddButton
+                    className={splitClasses([
+                        style[listType],
+                        style.placeholder__button
+                    ])}
+                    disabled
+                />{' '}
+                - to create note;
+            </ParagraphWithIllustration>
+            <ParagraphWithIllustration>
+                <DeleteNoteAllButton
+                    className={style.placeholder__button}
+                    disabled
+                />{' '}
+                - to delete all notes;
+            </ParagraphWithIllustration>
+            <ParagraphWithIllustration>
+                <DeleteNoteButton
+                    className={style.placeholder__button}
+                    disabled
+                />{' '}
+                - to delete note;
+            </ParagraphWithIllustration>
+            <ParagraphWithIllustration>
+                <SaveNoteButton
+                    className={style.placeholder__button}
+                    disabled
+                />{' '}
+                - to save note text.
+            </ParagraphWithIllustration>
+            <Paragraph>
+                You can also drag and drop notes between lists.
+            </Paragraph>
+        </div>
     </div>
 );
 
